b245e0f34bf28117b5ee5b6dff7e46ef99a86488
sousagomide <sousagomide@gmail.com>
2024-11-18 14:22:38 -0300

    upload

---
+++ apps/frontend/src/components/template/Logo.tsx
@@ -0,0 +1,20 @@
+import { Righteous } from 'next/font/google'
+import Image from 'next/image';
+import Link from 'next/link';
+
+const font = Righteous({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export default function Logo() {
+    return (
+        <Link href='/' className={`flex items-center gap-2 ${font.className}`}>
+            <Image src='/logo.svg' alt='Logo' width={50} height={50}/>
+            <h1 className='flex flex-col items-center text-lg leading-5'>
+                <div>CONVIT<span className='text-blue-500'>3</span></div>
+                <div>DIGITAL</div>
+            </h1>
+        </Link>
+    );
+}
---
+++ apps/frontend/src/components/template/Pagina.tsx
@@ -0,0 +1,17 @@
+import Logo from "./Logo";
+
+export interface PaginaProps {
+    children: React.ReactNode;
+    classname?:  string;
+}
+
+export default function Pagina(props: PaginaProps) {
+    return (
+        <div className="flex flex-col items-center py-10 min-h-screen bg-black bg-[url('/background.png')] bg-cover">
+            <Logo />
+            <main className={`flex-1 flex flex-col justify-center py-10  container ${props.classname}`}>
+                {props.children}
+            </main>
+        </div>
+    );
+}
